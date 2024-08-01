b7cfc85df6839ec0599a08e8865aa4bc89e4d5da
panyi <panyi@360.cn>
2024-08-01 10:50:51 +0800

    Feature v2.4.17 工作流测试

--- test.py
+++ test.py
@@ -1,8 +1,1 @@
-from fofa_hack import fofa
-def main():
-    result_generator = fofa.api("thinkphp", endcount=100)
-    for data in result_generator:
-        print(data)
-
-if __name__ == '__main__':
-    main()
+# edit test py
